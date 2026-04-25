371e6002f9ade3234a753b7dbb7628cf1a6583a9
FoggyCaligo <dkdlwkr16@gmail.com>
2026-04-25 19:44:15 +0900

    feat : 식재료 유통기한 경고 태그 추가

--- src/app/fridge/components/Ingredient.jsx
+++ src/app/fridge/components/Ingredient.jsx
@@ -1,21 +1,23 @@
 import Button from "@/components/ui/Button";
-import Card from "@/components/ui/Card";
 import Tag from "@/components/ui/Tag";
 
 export default function Ingredient({ children, variant = "primary", handleClickDelete, handleClickEdit, name, description, expires, qty }) {   
-
-    if(new Date(expires) - new Date() < 3){
-        
-    }
-
+    
+    const expirationDate = new Date(expires);
+    const today = new Date();
+    const diffInMs = expirationDate - today;
+    const isImminent = diffInMs < (3 * 24 * 60 * 60 * 1000);
+    const isExpired = expirationDate < today;
     return (
         <div className="flex flex-row justify-between items-center p-2 rounded-2xl border" style={{ backgroundColor: "var(--card-bg)", borderColor: "var(--border)" }}>
             <div className="flex-col ml-4">
                 <div className="text-lg font-bold">{name}</div>
-                <div className="text-sm text-gray-600">{description} {expires} {qty}</div>
+                <div className="text-sm text-gray-600">{description} {" . "} {expires} {" . "} {qty}</div>
             </div>
-            <div className="flex flex-row gap-2">
-                <Tag variant="accent">유통기한 임박</Tag>
+            <div className="flex flex-row gap-2 items-center">
+
+                {isImminent && <Tag variant="accent">유통기한 경고</Tag>}
+
                 <Button variant="secondary" is_square={true} handleClick={handleClickEdit}>
                     수정
                 </Button>
--- src/components/ui/Tag.jsx
+++ src/components/ui/Tag.jsx
@@ -2,25 +2,21 @@ import Tag from 'antd/es/tag';
 
 const CustomTag = ({ children, color, bgColor, variant, closable, onClose }) => {
 
-    const inlineStyle = {
-        primary: {
-            backgroundColor: "var(--primary)",
-        },
-        secondary: {
-            borderColor: "var(--border)",
-            color: "var(--text-sub)",
-        },
-        accent: {
-            backgroundColor: "var(--accent)",
-            color: "#fff",
-        },
+    const variantClasses = {
+        primary: "themePrimary",
+        secondary: "themeSecondary",
+        accent: "themeAccent",
     };
-	
-	return (
-        <Tag color={color} closable={closable} onClose={onClose} className="rounded-full w-fit max-h-fit px-3 py-0 text-xs flex items-center justify-center" style={inlineStyle[variant]}>
+    
+    return (
+        <Tag 
+            color={color} 
+            closable={closable} 
+            onClose={onClose} 
+            className={`rounded-full w-fit max-h-fit px-3 py-0 text-xs flex items-center justify-center ${variantClasses[variant] || ''}`}
+        >
             {children}
         </Tag>
-        
     );
 };
 
