8bc1bbbc4e2492d3c1a84cd583fd9e8a78bf1b2d
한수정 <qoddl159@nate.com>
2026-04-28 14:59:09 +0900

    Feat : 부족재료 -> 대체재료 확인

--- src/api/recipeApi.js
+++ src/api/recipeApi.js
@@ -2,7 +2,7 @@ import api from "@/config/axios";
 
 export const getAllRecipes = async () => {
     const response = await api.get("/recipes");
-    return response.data.data;
+    return response.data.data.content;
 };
 
 // export const getRecipeDetail = async (id) => {
