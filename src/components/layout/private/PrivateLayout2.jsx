2f338f99feb97cd430c25743f6ef55da75247064
feed-mina <myelin24@naver.com>
2026-05-02 14:23:51 +0900

    ingredients-price mockdata적용

--- src/components/layout/private/PrivateLayout2.jsx
+++ src/components/layout/private/PrivateLayout2.jsx
@@ -2,6 +2,7 @@ import Sidebar2 from "./Sidebar2";
 import PrivateHeader from "./PrivateHeader";
 import FloatingChatbot from "@/components/chat/FloatingChatbot";
 
+//  팀 코드 충돌 방지하기 위해 임의의 privateLayout2 생성
 export default function PrivateLayout2({ children }) {
     return (
         <div className="flex min-h-screen bg-[#fdfaf6] text-[#3f3a36]">
