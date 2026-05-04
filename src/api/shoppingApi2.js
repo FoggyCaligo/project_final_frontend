4aae0d9c21a74cc2e5269f559c582c27730c11ab
feed-mina <myelin24@naver.com>
2026-05-04 14:08:02 +0900

    feat: 로그인 이후 dashboard로 이동

--- src/api/shoppingApi2.js
+++
@@ -1,8 +0,0 @@
-import api from "@/config/axios";
-
-// 11번가 + 네이버쇼핑 연동 v2 엔드포인트
-export const shoppingApi = {
-  getFridgePrices: () => api.get("/v2/shopping/fridge/prices"),
-  getIngredientPrices: (ingredientId) =>
-    api.get(`/v2/shopping/ingredients/${ingredientId}/prices`),
-};
