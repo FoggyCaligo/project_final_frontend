08783ea690dbc41123f72f792afce9b3e084cacf
FoggyCaligo <dkdlwkr16@gmail.com>
2026-05-06 10:55:08 +0900

    feat : api 경로 수정

--- src/api/recommendApi.js
+++ src/api/recommendApi.js
@@ -1,7 +1,7 @@
 import api from "@/config/axios";
 
 export const getRecommendations = async (page = 0, size = 9) => {
-    const response = await api.get("/recipes/recommendations", {
+    const response = await api.get("/v1/recipes/recommendations", {
         params: { page, size },
     });
 
