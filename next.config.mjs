ad56b1520b56a0f556bf5d5b0b67d3838638d60e
FoggyCaligo <dkdlwkr16@gmail.com>
2026-05-07 18:21:55 +0900

    ngrok

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,25 @@ import { fileURLToPath } from "url";
 const projectRoot = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   turbopack: {
+//     root: projectRoot,
+//   },
+// };
+
+  
+
 const nextConfig = {
-  turbopack: {
-    root: projectRoot,
+   allowedDevOrigins: [
+    "smokiness-founding-verbalize.ngrok-free.dev",
+  ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
   },
 };
 
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:4040",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
